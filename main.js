14c1f81cf4c22e03b838e5941f40dae447294697
Medranomiler1 <medranomiler@gmail.com>
2022-12-23 23:03:27 -0600

    revised error messages, included error message for Not a Number entered in character length prompt

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// Travering the DOM to use elements in the JavaScript file
+// Traversing the DOM to use elements in the JavaScript file
 var result = document.getElementById('password');
 
 
-// putting random character functions into an object - refer to line 
+// putting random character functions into an object - refer to line 100 and beyond
 const randomCharacterFunction = {
 	lowercase: randomLower,
 	uppercase: randomUpper,
@@ -18,9 +18,9 @@ generate.addEventListener('click', generateThePassword);
 function generateThePassword(){
 
 // console log message to confirm that the button was clicked and function was triggered
-    console.log("the generate password button was clicked");
+    console.log("The generate password button was clicked");
 
-// declaring variable for length of password based on response to window alert and converting from a string to a number
+// declaring variable for the character length of password based on the user's response to the window alert, and converting the value from a string to a number
     var characterLength = Number(prompt("How many characters do you want in your password? Enter a NUMBER between 8 and 128: "));
 
 // declaring variables as booleans that are true or false based on responses to the window alerts
@@ -43,7 +43,7 @@ function generateThePassword(){
 		containsNumber, 
 		containsSymbol, 
 		characterLength
-		);
+	);
 };
 
 // generate password function
@@ -58,30 +58,34 @@ function generatePassword(lowercase, uppercase, number, symbol, characterLength)
 // count the number of desired character types and log to console.
 	const characterTypesCount = lowercase + uppercase + number + symbol;
 	console.log('The number of character types is: ' + characterTypesCount);
-// creating a character types array. This is an array of objects which have a value of true or false.
-	// using .filter method to filter out any types that have a value of false. in other words they are not desired character types in the password.
+// creating a character types array. This is an array of objects.
+	// using .filter method to filter out any types that have a value of false. In other words, they are not the character types chosen by the user to be included in the password.
 	const typesArr = [{lowercase}, {uppercase}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 
-// if statements for invalid inputs to window alerts such as invalid character length and no character types chosen. Displays error messages in the text box and asks the user to try again 
-
+// if statements for invalid inputs to window alerts such as invalid character length and no character types chosen. Displays error messages in the text box and window alerts, and asks the user to try again. The error messages are also logged in the console. 
 	// no character type was chosen. Cannot generate a password of any length if there is no character type criteria
 	if(characterTypesCount === 0) {
-		alert('Password not generated due to invalid character input. Try again, but make sure to choose at least 1 of the following character types to be included in your password: Uppercase, Lowercase, Numbers, Symbols. ')
-        console.log('Password not generated due to invalid character input');
+		alert('The password was not generated due to invalid character input. Try again, but make sure to choose at least 1 of the following character types to be included in your password: Uppercase, Lowercase, Numbers, Symbols. ')
+        console.log('The password was not generated due to invalid character input. No character types were chosen');
         result.style.color = "red";
-		return 'No password was generated due to missing character inputs. Please try again. The password must include at least one of the following: Uppercase, Lowercase, Numbers, Symbols.';
+		return 'The password was not generated due to invalid character input. Try again, but make sure to choose at least 1 of the following character types to be included in your password: Uppercase, Lowercase, Numbers, Symbols. ';
 
 	}
-	// invalid character length. Password character length must be between 8 and 128 characters  
+	// invalid character length. The value entered in reponse to the character length prompt was not a number between 8 and 128
 	if(characterLength < 8 || characterLength > 128) {
-		alert('Password not generated due to invalid character length input. Try again, but make sure the password length is between 8 and 128 characters.')
-        console.log('Password not generated due to invalid character length input');
+		alert('The password was not generated because the password character length provided was not a number between 8 and 128. Try again, but make sure the password character length is a NUMBER between 8 and 128')
+        console.log('The password was not generated because the password character length provided was not a number between 8 and 128.');
         result.style.color = "red";
-        return 'Please try again. The password length must be between 8 and 128 characters.'
+        return 'Please try again. The password was not generated because the password character length provided was not a number between 8 and 128.'
     }
-
-    
-// created a loop that increments by the number of character types count.
+	// the value entered in reponse to the character length prompt was not a number
+	if(isNaN(characterLength)) {
+		alert('The password was not generated because the password character length provided was not a number. Try again, but make sure the password character length is a NUMBER between 8 and 128.')
+        console.log('The password was not generated because the password character length provided was not a number.');
+        result.style.color = "red";
+        return 'Please try again. The password was not generated because the password character length provided was not a number. Make sure the password character length is a NUMBER between 8 and 128.'
+    }else{
+    // created a loop that increments by the number of character types count.
 	for(let i=0; i<characterLength; i+=characterTypesCount) {
 		typesArr.forEach(type => {
 			const functionName = Object.keys(type)[0];
@@ -91,14 +95,19 @@ function generatePassword(lowercase, uppercase, number, symbol, characterLength)
 
 // Here we are declaring the variable "password" as the result of generatedPassword function. We are also using the .slice method instead of the .vauleOf method because the for loop is incrementing by the number of character types. Ex: If using 4 different character types but your password length was not a factor of 4 (ex: 10), the password would end up being 12 characters long because it looped 3 times. The .slice method will remove the excess characters in that case.
 	var password = generatedPassword.slice(0, characterLength);
+
 	// valid password is displayed in green text and also logged in the console
     result.style.color = "green";
    	console.log("Congratulations! Your password is: " + password);
-	// returns the final password with the correct number of characters
+	// takes the unshuffled password (password) with the correct number of characters, and shuffles them so that it is not alternating based on character types. ex of unshuffled password w/ all four character types would alternate in the order: lower, upper, number, symbol, repeat.
+
+
+
 	return password;
-}
+}}
 
 // random character functions using CharCode map referenced from https://www.net-comber.com/charset.html. functions generate a number between 1 and 26 then add either 97, 65, or 48 to choose either a lowercase, uppercase, or a number. lowercase letters begin at 97, uppercase letters begin at 65, and numbers begin at 48. Symbols used are declared in string. 
+
 
 function randomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
